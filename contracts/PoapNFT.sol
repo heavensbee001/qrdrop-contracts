30213029a2621436ac0eaf3d5118455614301a90
heavensbee001 <heavensbee001@gmail.com>
2022-06-27 11:11:41 +0200

    Make PoapNFT soulbound + create _uri when deployed

--- contracts/PoapNFT.sol
+++ contracts/PoapNFT.sol
@@ -13,45 +13,66 @@ contract PoapNFT is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
+  string private _uri;
+
+  mapping (address=>uint) private _ownerToTokenId;
+
   event NewPoapNFTMinted(address sender, uint256 tokenId);
 
   // We need to pass the name of our NFTs token and it's symbol.
-  constructor(string memory _name, string memory _symbol) ERC721 (_name, _symbol) {
+  constructor(string memory _name, string memory _symbol, string memory _description, string memory _imageUri) ERC721 (_name, _symbol) {
     console.log("initializing Poap NFT Contract");
+    _tokenIds.increment();
+
+    // store Poap uri 
+    string memory json = Base64.encode(
+        bytes(
+            string(
+                abi.encodePacked(
+                    '{"name": "', _name,'","description": "', _description,'","image": "', _imageUri,'"}'
+                )
+            )
+        )
+    );
+
+    _uri = string(
+        abi.encodePacked("data:application/json;base64,", json)
+    );
+    
   }
 
   // A function our user will hit to get their NFT.
-  function makeAPoapNFT(string memory _name, string memory _description, string memory _imageUri) public {
+  function makeAPoapNFT() public {
      // Get the current tokenId, this starts at 0.
     uint256 newItemId = _tokenIds.current();
 
      // Actually mint the NFT to the sender using msg.sender.
     _safeMint(msg.sender, newItemId);
 
-    string memory json = Base64.encode(
-        bytes(
-            string(
-                abi.encodePacked(
-                    '{"name": "', _name,',"description": "', _description,',"image": "', _imageUri,'"}'
-                )
-            )
-        )
-    );
-
-    string memory finalTokenUri = string(
-        abi.encodePacked("data:application/json;base64,", json)
-    );
-
     console.log("\n--------------------");
-    console.log(string(abi.encodePacked("https://nftpreview.0xdev.codes/?code=", finalTokenUri)));
+    console.log(string(abi.encodePacked("https://nftpreview.0xdev.codes/?code=", _uri)));
     console.log("--------------------\n");
+    
     // Set the NFTs data.
-    _setTokenURI(newItemId, finalTokenUri);
+    _setTokenURI(newItemId, _uri);
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
 
     // Increment the counter for when the next NFT is minted.
     _tokenIds.increment();
 
     emit NewPoapNFTMinted(msg.sender, newItemId);
+  }
+
+  function transferFrom() public pure {
+    revert('This token is not transferable');
+  }
+  function safeTransferFrom() public pure {
+    revert('This token is not transferable'); 
+  }
+  function approve() public pure {
+    revert('This token is not transferable'); 
+  }
+  function getApproved() public pure {
+    revert('This token is not transferable'); 
   }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -19,31 +19,26 @@ const createCreatorNftContract = async () => {
     );
     // Wait for it to be mined.
     await txn.wait();
-
-    // txn = await nftContract.makeACreatorNFT(
-    //     "Second",
-    //     "image.jpg",
-    //     "This is the second creator nft of the contract."
-    // );
-    // // Wait for it to be mined.
-    // await txn.wait();
 };
 const createPoapNftContract = async () => {
+    // const [accountA, accountB] = await hre.ethers.getSigners();
+
     const nftContractFactory = await hre.ethers.getContractFactory("PoapNFT");
     const nftContract = await nftContractFactory.deploy(
         "Poap NFT Contract",
-        "PNC"
+        "PNC",
+        "This is the poap nft.",
+        "poapimage.jpg"
     );
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
 
     // Call the function.
-    let txn = await nftContract.makeAPoapNFT(
-        "First Poap",
-        "This is the first poap nft of the contract.",
-        "poapimage.jpg"
-    );
+    let txn = await nftContract.makeAPoapNFT();
     // Wait for it to be mined.
+    await txn.wait();
+
+    txn = await nftContract["safeTransferFrom()"]();
     await txn.wait();
 };
 
